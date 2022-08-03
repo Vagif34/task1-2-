5d098c94ffdfcbe85f69337e4f12724eeb6a3af0
my file <vagifrg@code.edu.az>
2022-08-03 18:07:42 +0400

    creat file

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 23, 34, 5, 6, 44 };
+            int min = arr[0];
+            for (int i = 0; i <arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
